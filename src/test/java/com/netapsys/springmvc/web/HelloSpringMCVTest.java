080fa769d13df478291cc998b8fa60d1487894d3
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-04-04 11:27:31 +0200

    pom.xml : junit + spring

---
+++ src/test/java/com/netapsys/springmvc/web/HelloSpringMCVTest.java
@@ -0,0 +1,11 @@
+package com.netapsys.springmvc.web;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: quentindecoudier
+ * Date: 04/04/12
+ * Time: 11:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloSpringMCVTest {
+}
