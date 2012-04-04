2454e313ed5e9c217ae624fc25122c9bf9255d30
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-04-04 11:30:21 +0200

    test spring mvc

--- src/test/java/com/netapsys/springmvc/web/HelloSpringMCVTest.java
+++ src/test/java/com/netapsys/springmvc/web/HelloSpringMCVTest.java
@@ -1,11 +1,20 @@
 package com.netapsys.springmvc.web;
 
-/**
- * Created by IntelliJ IDEA.
- * User: quentindecoudier
- * Date: 04/04/12
- * Time: 11:19
- * To change this template use File | Settings | File Templates.
- */
-public class HelloSpringMCVTest {
+import org.springframework.ui.ModelMap;
+
+import junit.framework.TestCase;
+
+public class HelloSpringMCVTest extends TestCase {
+
+  public void testSayHelloWithSpringMVC(){
+    HelloSpringMVC helloSpringMVC = new HelloSpringMVC();
+    ModelMap model = new ModelMap();
+    String view = helloSpringMVC.sayHelloWithSpringMVC("mic", model);
+    //vérifions que c'est bien la vue hello qui est générée
+    assertEquals("hello", view);
+    //vérifions que le modele contient bien un attribut name
+    assertTrue(model.containsAttribute("name"));
+    //vérifions que cet attribut name est bien "mic"
+    assertEquals("mic",model.get("name"));
+  }
 }
