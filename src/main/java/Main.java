61a82e1afd6b00720da2eb7c166fd1c9e1959d1a
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-04-04 09:27:48 +0200

    test joda-time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import org.joda.time.DateTime;
+
 public class Main {
     public static void main (String[] args){
+        DateTime dt = new DateTime();
+        System.out.println(dt.getMonthOfYear());
         System.out.println("Hello World");
     }
 }
