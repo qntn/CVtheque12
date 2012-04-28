df6f01cbbb53a7e5cebef157a13fe278408badda
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-04-28 17:47:58 +0200

    changement de strategie -> commencement d'un projet rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvtheque12::Application.config.secret_token = '82930807258aaf6cf0c4bb6be762857f8e174bb20121e74d9f6e8aec1b85f16d79526d38db0e59757158db0a6cd83abbc93efe3be7454902d1d00921fd0c5ab6'
--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.joda.time.DateTime;
-
-public class Main {
-    public static void main (String[] args){
-        DateTime dt = new DateTime();
-        System.out.println(dt.getMonthOfYear());
-        System.out.println("Hello World");
-    }
-}
