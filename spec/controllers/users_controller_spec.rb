104f49f4b6cbd06f86060a47f6d0169fd6717223
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-05-09 00:32:13 +0200

    supression du test de présence de l'action show pour le controller de User

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe UsersController do
-  it "should get show" do
-    get "show"
-    response.should be_success
-  end
-
   it "should get edit" do
     get "edit"
     response.should be_success
