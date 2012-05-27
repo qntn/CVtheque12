94a96034efecd18e5b2267c1b7525ac29c0ec8a0
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-05-27 23:27:42 +0200

    modification sur les tests du controller User

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,23 +3,14 @@ require 'spec_helper'
 describe UsersController do
   before(:each) do
     @user = FactoryGirl.create :user
-  end
-  it "should get edit" do
-    get "edit"
-    response.should be_success
-  end
-  it "should assign user" do
-    get "edit"
-    assigns(:user).should eq(@user)
+    sign_in @user
   end
   it "should put update" do
     cv = Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/cv2.pdf", 'application/pdf', true)
-    put :update, :id => @user.id, :user => { :cv => cv }
-    assigns(:user).should eq(@user)
-    response.should redirect_to edit_user_url @user
+    put :update, :user => { :cv => cv }
+    response.should redirect_to edit_user_registration_path
   end
   it "should be able to log in" do
-    sign_in @user
     subject.current_user.should_not be_nil
   end
 end
